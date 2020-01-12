3fc23718c4d60343af72634bce3f310df4068c4c
김건우 <kgw1457@gamil.com>
2020-01-13 01:24:17 +0900

    Add aouth google login

--- src/main/java/com/example/blog/springboot/web/IndexController.java
+++ src/main/java/com/example/blog/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.example.blog.springboot.web;
 
+import com.example.blog.springboot.config.auth.LoginUser;
+import com.example.blog.springboot.config.auth.dto.SessionUser;
+import com.example.blog.springboot.domain.user.User;
 import com.example.blog.springboot.service.posts.PostsService;
 import com.example.blog.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +11,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
 
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
 
     }
